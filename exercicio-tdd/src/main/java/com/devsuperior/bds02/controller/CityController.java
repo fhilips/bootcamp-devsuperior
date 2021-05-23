e0fad980b0bdc64dfddca529d68fca1c8c17a64a
filipe santos <fhilips.souza@gmail.com>
2021-05-23 14:45:18 -0300

    Add: end point find all sorted by name

---
+++ exercicio-tdd/src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.bds02.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bds02.dto.CityDTO;
+
+import com.devsuperior.bds02.service.CityService;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+	@Autowired
+	private CityService service;
+	
+	@GetMapping
+	public ResponseEntity<List<CityDTO>> findAllSortedByName(){
+		List<CityDTO> entity = service.findAllSortedByName();	
+		
+		return ResponseEntity.ok(entity);	
+	}
+}
---
+++ exercicio-tdd/src/main/java/com/devsuperior/bds02/controller/EventController.java
@@ -0,0 +1,5 @@
+package com.devsuperior.bds02.controller;
+
+public class EventController {
+
+}
